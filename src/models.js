7fdf94e03704ba13cbb2a513a16d20d23186a740
okay <okay@chalk>
2019-03-18 20:24:22 -0700

    [replays] save replays in a more performant way

--- src/models.js
+++ src/models.js
@@ -54,6 +54,7 @@ var PostOpBlob = replaydb.define('PostOpBlob', {
 var force_reset = process.env.RESET;
 sequelize.sync({ force: force_reset }).then(function() {
   console.log("Synced SQL DB to models");
+
 });
 replaydb.sync({ force: force_reset }).then(function() {
   console.log("Synced SQL ReplayDB to models");
@@ -62,7 +63,7 @@ replaydb.sync({ force: force_reset }).then(function() {
 
 var zlib = require("zlib");
 function getPostOps(randid, cb) {
-  PostOp.findAll({where: { randid: randid}}).then(function(changes) {
+  PostOp.findAll({where: { randid: randid}, raw: true}).then(function(changes) {
     PostOpBlob.findOrCreate({where: {randid: randid}}).then(function(blobs) {
       var blob = blobs[0];
 
@@ -72,10 +73,12 @@ function getPostOps(randid, cb) {
       }
 
       var dirty = false;
+      var del = [];
       for (var c in changes) {
         var change = changes[c];
-        blobChanges.push(change.toJSON());
-        change.destroy();
+        blobChanges.push(change);
+        change.change = JSON.parse(change.change);
+        del.push(change.id);
         dirty = true;
       }
 
@@ -92,15 +95,48 @@ function getPostOps(randid, cb) {
         }
       }
 
-      cb(blobChanges);
-
       if (dirty) {
         blob.changes = zlib.gzipSync(JSON.stringify(blobChanges));
-        blob.save();
+        blob.save().then(function() {
+          console.log("SAVED BLOB", randid)
+          if (del.length > 0) {
+            PostOp.destroy({where: { id: del }}).then(function() {
+              cb(blobChanges);
+            });
+          }
+        });
+      } else {
+        cb(blobChanges);
       }
+
     });
   });
 }
+
+function packPostOps() {
+  function packAndMoveOn(rows) {
+    var i = 0;
+    function next() {
+      if (i >= rows.length) { return; }
+
+      var row = rows[i];
+      var randid = row.DISTINCT;
+      i++;
+      getPostOps(randid, function() {
+        setTimeout(next, 100);
+      });
+    }
+
+    next();
+  }
+
+  PostOp.aggregate('randid', 'DISTINCT', { plain: false })
+    .then(function(rows) {
+      packAndMoveOn(rows);
+    });
+}
+
+setInterval(packPostOps, 5 * 60 * 1000); // every 5 minutes we pack
 
 // all models defined need to be required somewhere before the main setup is called
 module.exports.instance = sequelize;
